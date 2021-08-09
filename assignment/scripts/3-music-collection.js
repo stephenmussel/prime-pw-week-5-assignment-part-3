86f4529b39cb9d2f9da29f123b5401b9c87615c6
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-08-09 18:55:50 -0500

    created object (album) with properties of function (addToCollection)

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,6 +2,12 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+let album = {
+  title: 'title',
+  artist: 'artist',
+  yearPublished: 1999
+};
+
 function addToCollection(title, artist, yearPublished) {
 
 }
