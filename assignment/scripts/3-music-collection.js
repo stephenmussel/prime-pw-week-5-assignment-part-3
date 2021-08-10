737157d1451354b6a8e9beee83dced2d4671548c
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-08-09 19:08:47 -0500

    added album to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,11 +3,14 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 let album = {
-  title: 'title',
-  artist: 'artist',
-  yearPublished: 1999
+  title: 'ok computer',
+  artist: 'radiohead',
+  yearPublished: 1997
 };
 
 function addToCollection(title, artist, yearPublished) {
-
+  collection.push(album);
+    return album;
 }
+
+addToCollection();
