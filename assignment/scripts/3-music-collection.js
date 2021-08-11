2d9d370985a079ee60611d4694b7f10945dcbb03
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-08-10 23:08:17 -0500

    formatted showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,32 +1,27 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
-
-let album = {
-  title: 'a radical recital',
-  artist: 'rasputina',
-  yearPublished: 2005
-}
+let collection = []; // starts as empty array
 
 function addToCollection(title, artist, yearPublished) {
-  collection.push({title, artist, yearPublished}); // add album to end of collection array
-  return {title, artist, yearPublished}; // returns values of album object
+    collection.push({title, artist, yearPublished}); // adds album to end of collection
+    return{title, artist, yearPublished}; // returns album properties
 } // end addToCollection
 
-console.log(addToCollection('radical recital', 'rasputina', 2005)); // logs one album
-console.log(addToCollection('the lost & found 2nd edition', 'rasputina', 2007)); // logs two album
-console.log(addToCollection('comment te dire adieu', 'francoise hardy', 1968)); // logs three album
-console.log(addToCollection('juno soundtrack', 'barry louis polisar', 2007)); // logs four album
-console.log(addToCollection('garden party', 'ricky nelson', 1972)); // logs five album
-console.log(addToCollection('tous les garcon et les filles', 'francoise hardy', 1962)); // logs six album
+console.log(addToCollection('a radical recital', 'rasputina', 2005)); // logs 1st album added to collection
+console.log(addToCollection('the lost & found 2nd edition', 'rasputina', 2007)); // logs 2nd album added to collection
+console.log(addToCollection('comment te dire adieu', 'francoise hardy', 1968)); // logs 3rd album added to collection
+console.log(addToCollection('juno soundtrack', 'barry louis polisar', 2007)); // logs 4th album added to collection
+console.log(addToCollection('garden party', 'ricky nelson', 1972)); // logs 5th album added to collection
+console.log(addToCollection('tous les garcon et les filles', 'francoise hardy', 1962)); // logs 6th album added to collection
 console.log(collection); // logs collection of albums
 
-function showCollection() { // take in an array parameter
-  // console.log(`the number of albums in this collection is: ${collection.length}`); // log number of items (albums) in array (collection)
-
-  for (let albumInfo in album) {
-    console.log(`${albumInfo}: ${album[albumInfo]}`);
+function showCollection() {
+  // takes in any parameters
+  console.log(`the number of albums in this collection is: ${collection.length}`); //logs number of items in array
+  for (let i=0; i<collection.length; i++) {
+    console.log(`${collection[i].title} by ${collection[i].artist} published in ${collection[i].yearPublished}`);
   }
 } // end showCollection
 
-showCollection();
+// used this to test formatting: console.log(`${collection[0].title} by ${collection[0].artist} published in ${collection[0].yearPublished}`);
+console.log(showCollection());
