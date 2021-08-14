68d30efeb99a846d926d2764f54e713c510e9c34
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-08-13 22:20:21 -0500

    added more comments to file and testing search function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,6 +2,11 @@ console.log('***** Music Collection *****')
 
 let collection = []; // starts as empty array
 
+/*  FUNCTION: addToCollection()
+  * INPUT: object {string, string, number}
+  * OUTPUT: object {string, string, number} from array
+*/
+
 function addToCollection(title, artist, yearPublished) {
     // let album = {title, artist, yearPublished}; // another method to create new object
     let album = {
@@ -27,6 +32,12 @@ console.log(addToCollection('garden party', 'ricky nelson', 1972)); // logs 5th 
 console.log(addToCollection('tous les garcon et les filles', 'francoise hardy', 1962)); // logs 6th album added to collection
 console.log(collection); // logs collection of albums
 
+/*  FUNCTION: showCollection()
+  * INPUT: array []
+  * OUTPUT: number
+  * OUTPUT: object {string, string, number}
+*/
+
 function showCollection(arrParameter) { // takes in array parameters
   console.log(`the number of albums in this collection is: ${collection.length}`); //logs number of items in array
   for (let i=0; i<collection.length; i++) {
@@ -40,19 +51,28 @@ console.log(showCollection()); // test to log each album: TITLE by ARTIST publis
 // used this to test formatting: console.log(`${collection[0].title} by ${collection[0].artist} published in ${collection[0].yearPublished}`);
 // console.log(showCollection()); // outputs: a radical recital by rasputina published in 2005
 
+/*  FUNCTION: findByArtist()
+  * INPUT: string
+  * OUTPUT: string or empty array
+*/
+
 function findByArtist(artist) {
   let artistArr = [];
   for (let i =0; i<collection.length; i++) {
     if (artist === collection[i].artist) { // if a string === collection[i].artist
-      artistArr.push({artist}); // push a string to artistArr
+      artistArr.push({artist}); // pushes passed argument to artistArr
     }
-  }
-  return artistArr;
+  } return artistArr;
 } // end findByArtist
 
 console.log(findByArtist('test')); // outputs empty array
 console.log(findByArtist('rasputina')); // outputs 2 object properties into array
 console.log(findByArtist('ricky nelson')); // outputs 1 object property into array
+
+/*  FUNCTION: search()
+  * INPUT: object {string, number}
+  * OUTPUT:
+*/
 
 function search(artist, year) {
   let searchArr = [];
@@ -60,7 +80,10 @@ function search(artist, year) {
     if (artist === collection[i].artist && year === collection[i].yearPublished) {
       searchArr.push(artist, year);
     }
-  } return searchArr;
+    return searchArr;
+  } console.log(collection);
 }
 
-console.log(search('rasputina', 2005));
+console.log(search('rasputina', 2005)); // tests for object properties found in collection. outputs empty array
+console.log(search('rasputina', 2006)); // tests for object one object property not found it collection. outputs empty array
+console.log(search()); // tests for no search object. outputs all albums in collection REVISIT THIS, NOT WORKING
